7d29761020f34ec98195b5c348da7c5a5e0e571e
Tkachenko Yevhen <zekatka01@gmail.com>
2022-08-19 20:28:33 +0200

    comp card drawer header

---
+++ src/components/Header.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="d-flex justify-between align-center p-40">
+            <div className="d-flex align-center">
+                <img width={40} height={40} src="/img/logo.png" alt="logo"/>
+                <div>
+                    <h3 className="text-uppercase">React Sneakers</h3>
+                    <p className="opacity-5">Магазин лучших кросовок</p>
+                </div>
+            </div>
+
+            <ul className="headerRight d-flex">
+                <li className="mr-30">
+                    <img width={20} height={20} src="/img/cart.svg" alt="cart"/>
+                    <span>
+                    1205 грн.
+                </span>
+                </li>
+                <li>
+                    <img width={20} height={20} src="/img/user.svg" alt="user"/>
+                </li>
+            </ul>
+
+        </header>
+    );
+};
+
+export default Header;
